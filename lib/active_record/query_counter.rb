6056e7bd9a53a85cd6624624843a3095d9544567
cedricboulanger <cboulanger@simcotechnologies.com>
2015-12-17 16:42:24 -0500

    add output of sql in debug mode

--- lib/active_record/query_counter.rb
+++ lib/active_record/query_counter.rb
@@ -31,15 +31,20 @@ module ActiveRecord
       (@query_counter || {}).sum { |key, value| (COUNT_QUERY_TYPE.include?(key) && sql_operation.nil?) || sql_operation == key ? value : 0 }
     end
 
-    def within
+    def within output_queries: false
+      @output_queries = output_queries
+
       restart
       yield
       stop
+      nil # force nil to be returned
     end
 
     private
     def increment sql
       if started?
+        puts " ** QueryCounter debug: #{sql}" if @output_queries
+
         sql_operation = sql.split[0].to_s.upcase
         ((@query_counter ||= {})[sql_operation] ||= 0)
         @query_counter[sql_operation] = @query_counter[sql_operation] + 1
